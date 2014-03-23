a73f1297c44237314cebdfc5bdce48e5a05dd004
Andrew Van Tassel <andrew@andrewvantassel.com>
2014-03-23 13:26:41 -0600

    Testing angular cartodb

--- js/main.js
+++ js/main.js
@@ -23,3 +23,57 @@ $(document).ready(function() {
 	});
 
 });
+
+var sensusApp = angular.module('senseus', ['ngRoute']);
+
+sensusApp.config(function($routeProvider) {
+		$routeProvider
+
+			// route for the home page
+			.when('/', {
+				templateUrl : 'templates/home.html',
+				controller  : 'homeController'
+			});
+	});
+
+sensusApp.controller('homeController', function($scope) {	
+	// http://developers.cartodb.com/documentation/cartodb-js.html
+  var views = {
+  	'age': {url:'http://senseus.cartodb.com/api/v2/viz/3f4fc986-b243-11e3-a13e-0e10bcd91c2b/viz.json'}
+  	,'income': {url:'http://senseus.cartodb.com/api/v2/viz/2e84858a-b241-11e3-be2e-0e73339ffa50/viz.json'}
+  	,'population': {url:'http://senseus.cartodb.com/api/v2/viz/a5f3a766-b243-11e3-9c07-0e10bcd91c2b/viz.json'}
+  	,'edu': {url:'http://senseus.cartodb.com/api/v2/viz/c495a460-b23b-11e3-8f25-0e230854a1cb/viz.json'}
+  }	;
+  
+  var map = null;
+
+  $scope.changeView = function(view){
+  	console.log('changeView',view, views[view].url);
+
+  	cartodb.createVis('map', views[view].url, {
+            zoom: 9
+        })
+        .done(function(vis, layers) {
+          // layer 0 is the base layer, layer 1 is cartodb layer
+          // setInteraction is disabled by default
+          layers[1].setInteraction(true);
+          // layers[1].on('featureOver', function(e, pos, latlng, data) {
+          //   cartodb.log.log(e, pos, latlng, data);
+          // });
+
+          // you can get the native map to work with it
+          // depending if you use google maps or leaflet
+          map = vis.getNativeMap();
+
+          // now, perform any operations you need
+          // map.setZoom(3)
+          // map.setCenter(new google.maps.Latlng(...))
+        })
+        .error(function(err) {
+          console.log(err);
+        });
+  }
+
+  $scope.changeView('income');
+    
+});
