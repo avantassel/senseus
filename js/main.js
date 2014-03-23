e4a06b6d9afa6ec71ec70a71a380a393af59d9c5
Andrew Van Tassel <andrew@andrewvantassel.com>
2014-03-23 15:19:22 -0600

    Added list view and toggle map/list views

--- js/main.js
+++ js/main.js
@@ -1,11 +1,3 @@
-function loadIframe(iframeName, url) {
-    var $iframe = $('#' + iframeName);
-    if ( $iframe.length ) {
-        $iframe.attr('src',url);   
-        return false;
-    }
-    return true;
-}
 
 $(document).ready(function() {
 
@@ -15,16 +7,18 @@ $(document).ready(function() {
 
 		var newData = $(this).text();
 		$('#first').html(newData + ' <span class="glyphicon glyphicon-remove-sign"></span>');
+		return false;
 	});
 
 	$('.big span').on('click', function() {
 		$('.big span').removeClass('active');
 		$(this).addClass('active');
+		return false;
 	});
 
 });
 
-var sensusApp = angular.module('senseus', ['ngRoute']);
+var sensusApp = angular.module('senseus', ['ngRoute','ngTable']);
 
 sensusApp.config(function($routeProvider) {
 		$routeProvider
@@ -36,23 +30,41 @@ sensusApp.config(function($routeProvider) {
 			});
 	});
 
-sensusApp.controller('homeController', function($scope) {	
+sensusApp.factory('MapService', function($http) {
+  
+  return {
+
+    search: function (table,callback){
+      var params = {format:'GeoJSON','q':'select * from '+table+' limit 10'};
+      $http.get('http://senseus.cartodb.com/api/v2/sql/',{params: params}).then(function(response){
+          //return results
+          callback(response.data);
+        }, function(response){
+          //throw error
+          console.log('Error',response);
+        });
+    }
+};
+});
+
+sensusApp.controller('homeController', function($scope, MapService, ngTableParams) {	
 	// http://developers.cartodb.com/documentation/cartodb-js.html
   var views = {
-  	'age': {url:'http://senseus.cartodb.com/api/v2/viz/3f4fc986-b243-11e3-a13e-0e10bcd91c2b/viz.json'}
-  	,'income': {url:'http://senseus.cartodb.com/api/v2/viz/2e84858a-b241-11e3-be2e-0e73339ffa50/viz.json'}
-  	,'population': {url:'http://senseus.cartodb.com/api/v2/viz/a5f3a766-b243-11e3-9c07-0e10bcd91c2b/viz.json'}
-  	,'edu': {url:'http://senseus.cartodb.com/api/v2/viz/c495a460-b23b-11e3-8f25-0e230854a1cb/viz.json'}
+  	'age': {url:'http://senseus.cartodb.com/api/v2/viz/3f4fc986-b243-11e3-a13e-0e10bcd91c2b/viz.json', table: 'census'}
+  	,'income': {url:'http://senseus.cartodb.com/api/v2/viz/2e84858a-b241-11e3-be2e-0e73339ffa50/viz.json', table: 'census'}
+  	,'population': {url:'http://senseus.cartodb.com/api/v2/viz/a5f3a766-b243-11e3-9c07-0e10bcd91c2b/viz.json', table: 'census'}
+  	,'edu': {url:'http://senseus.cartodb.com/api/v2/viz/c495a460-b23b-11e3-8f25-0e230854a1cb/viz.json', table: 'census'}
   }	;
   
-  var map = null, viz = null;
+  $scope.map = null, $scope.vis = null, $scope.results = null;
 
   function initView(view){
 
   	console.log('initView',view);
   	cartodb.createVis('map', views[view].url, {
             zoom: 9,
-            legends: true
+            legends: true,
+            zoomControl: true
         })
         .done(function(vis, layers) {
           // layer 0 is the base layer, layer 1 is cartodb layer
@@ -64,8 +76,8 @@ sensusApp.controller('homeController', function($scope) {
 
           // you can get the native map to work with it
           // depending if you use google maps or leaflet
-          map = vis.getNativeMap();
-          viz = vis;
+          $scope.map = vis.getNativeMap();
+          $scope.vis = vis;
 
           // now, perform any operations you need
           // map.setZoom(3)
@@ -74,18 +86,27 @@ sensusApp.controller('homeController', function($scope) {
         .error(function(err) {
           console.log(err);
         });
+        MapService.search(views[view].table,function(data){
+          	loadTable(data.features);
+          });
+  }
+
+  function loadTable(data){
+		
+		$scope.tableParams = new ngTableParams({
+	        page: 1,            // show first page
+	        count: 10           // count per page
+	    }, {
+	        total: data.length, // length of data
+	    });
+		$scope.results = data;
   }
 
   $scope.changeView = function(view){
   
-  	console.log('changeView');
-
-  	var layers = viz.getLayers();
-  	console.log('Layers',layers);
-
-          // create layer and add to the map, then add some intera
-          cartodb.createLayer(map, views[view].url)
-          .addTo(map)
+  	      // create layer and add to the map, then add some intera
+          cartodb.createLayer($scope.map, views[view].url)
+          .addTo($scope.map)
           .on('done', function(layer) {
             // var sublayer = layer.getSubLayer(0);
             // sublayer.on('featureOver', function(e, pos, latlng, data) {
@@ -100,6 +121,21 @@ sensusApp.controller('homeController', function($scope) {
           .on('error', function() {
             cartodb.log.log("some error occurred");
           });
+
+          MapService.search(views[view].table,function(data){
+          	console.log(data.features);
+          	loadTable(data.features);
+          });
+	}
+	$scope.toggleView = function(){
+		if($('.map').is(':hidden')){
+			$('.list').hide();
+			$('.map').show();
+		} else {
+			$('.map').hide();
+			$('.list').show();
+		}
+
 	}
 
   initView('income');
